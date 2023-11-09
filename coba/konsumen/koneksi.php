ad76b341e615019c2de1e2d286658810c0f1b2e2
easti <eastidmyt@gmail.com>
2023-11-09 14:52:44 +0700

    tinggal modal kelola produk

--- coba/konsumen/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $host = "localhost";
-    $user = "Chandra";
-    $pass = "chandraapriyanto";
-    $database = "proyek1";
-    $koneksi = mysqli_connect($host, $user, $pass, $database);
-?>
